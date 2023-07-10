d55945e0fb837164725038823e11701dca315caf
BeniSyach <benisyach32@gmail.com>
2023-07-10 12:20:08 +0700

    add css CKAN

--- pages/datasets.tsx
+++ pages/datasets.tsx
@@ -1,7 +1,5 @@
 import getConfig from "next/config";
-import Link from "next/link";
 import React, { useContext, useState } from "react";
-import Organisasi from "./organisasi";
 import {
   CKAN,
   DatasetSearchForm,
@@ -21,18 +19,6 @@ const formatter = new Intl.DateTimeFormat(["ban", "id"], {
 });
 
 export async function getServerSideProps() {
-  const response = await fetch(`${dms}/api/3/action/package_search`);
-  const datasets = await response.json();
-  const datasetsWithDetails = await Promise.all(
-    datasets.result.results.map(async (dataset) => {
-      const response = await fetch(
-        `${dms}/api/3/action/package_show?id=` + dataset.name
-      );
-      const json = await response.json();
-      return json.result;
-    })
-  );
-
   const getDataOrganisasi = await fetch(
     `${dms}/api/3/action/organization_list?all_fields=true`
   );
@@ -57,7 +43,6 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      datasets: datasetsWithDetails,
       organisasi: Organisasi,
       grup: Grup,
       tags: Tags,
@@ -70,7 +55,6 @@ export async function getServerSideProps() {
 export default function datasetsPage({
   orgs,
   groups,
-  datasets,
   organisasi,
   grup,
   tags,
@@ -116,62 +100,6 @@ export default function datasetsPage({
           </div>
           <div className="card w-full justify-center items-center shadow-2xl bg-base-100">
             <div className="card-body flex lg:flex-row md:flex-col sm:flex-col">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Cari Datasets</span>
-                </label>
-              </div>
-              {/* <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Group</span>
-                </label>
-                <select
-                  className="select select-bordered w-full max-w-xs"
-                  value={selectedGrup}
-                  onChange={handleSetSelectedGrup}
-                >
-                  {grup && grup.length > 0 ? (
-                    <>
-                      <option>Pilih Grup</option>
-                      {grup.map((datagrup, k) => (
-                        <option key={k} value={datagrup.name}>
-                          {datagrup.title}
-                        </option>
-                      ))}
-                    </>
-                  ) : (
-                    <option disabled value="0">
-                      Data Tidak Ada
-                    </option>
-                  )}
-                </select>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Organisasi</span>
-                </label>
-                <select
-                  className="select select-bordered w-full max-w-xs"
-                  value={selectedOrganisasi}
-                  onChange={handleSetSelectedOrganisasi}
-                >
-                  <option selected>Pilih Organisasi</option>
-                  {organisasi && organisasi.length > 0 ? (
-                    <>
-                      {organisasi.map((dataOrganisasi, k) => (
-                        <option key={k} value={dataOrganisasi.name}>
-                          {dataOrganisasi.title}
-                        </option>
-                      ))}
-                    </>
-                  ) : (
-                    <option value="0">Data Tidak Ada</option>
-                  )}
-                </select>
-              </div>
-              <div className="form-control mt-9">
-                <button className="btn btn-primary">Cari</button>
-              </div> */}
               <DatasetSearchForm
                 options={options}
                 setOptions={setOptions}
@@ -185,7 +113,7 @@ export default function datasetsPage({
       <div className="bg-base-200">
         <div className="container mx-auto">
           <div className="flex flex-col w-full lg:flex-row py-12">
-            <div className="flex h-auto flex-grow card bg-base-300 rounded-box place-items-start">
+            <div className="flex h-auto flex-grow card bg-base-300 rounded-box place-items-start w-1/4">
               <div className="judul-item flex flex-col text-start p-5">
                 <p className="text-md font-bold p-2">
                   Cari Berdasarkan Organisasi
@@ -253,67 +181,14 @@ export default function datasetsPage({
               )}
             </div>
             <div className="divider divider-horizontal"></div>
-            <div className="grid h-auto  card bg-base-300 rounded-box place-items-start">
-              {/* <div className="judul-item">
-                <p className="flex text-4xl font-bold text-md p-5">
-                  {datasets.length} Datasets
-                </p>
-                <div className="judul-item flex flex-col text-center p-4 ">
-                  {datasets && datasets.length > 0 ? (
-                    <div>
-                      {datasets.map((dataset, k) => (
-                        <Link
-                          key={k}
-                          href={`/@${dataset.organization.name}/${dataset.name}`}
-                        >
-                          <div className="card card-side bg-base-100 shadow-xl w-full mb-5 cursor-pointer hover:bg-base-200">
-                            <figure>
-                              <img
-                                src="https://deliserdangkab.go.id/files/website_resmi.png"
-                                alt="logo Datasets"
-                                // className="lg:w-[200px] sm:w-[100px] p-5"
-                                className="w-full"
-                              />
-                            </figure>
-                            <div className="card-body">
-                              <h5 className="card-title text-start">
-                                {dataset.title}
-                              </h5>
-                              <p className="text-start text-sm">
-                                {dataset.notes}
-                              </p>
-                              <div className="card-actions justify-start">
-                                <span className="badge badge-lg badge-primary text-sm">
-                                  {dataset.organization.title}
-                                </span>
-                                <span className="badge badge-lg badge-primary text-sm">
-                                  {formatter.format(
-                                    new Date(dataset.metadata_modified)
-                                  )}
-                                </span>
-                              </div>
-                            </div>
-                          </div>
-                        </Link>
-                      ))}
-                    </div>
-                  ) : (
-                    <p className="bg-base-300 m-5 p-5 text-4xl font-bold ">
-                      Tidak Ada Datasets
-                    </p>
-                  )}
-                </div>
+            <div className="grid h-auto  card bg-base-300 rounded-box place-items-start w-10/12">
+              <div className="mx-10 my-5">
+                <ListOfDatasets
+                  options={options}
+                  setOptions={setOptions}
+                  ckan={ckan}
+                />{" "}
               </div>
-              <div className="btn-group flex p-5">
-                <button className="btn">«</button>
-                <button className="btn">Halaman 1</button>
-                <button className="btn">»</button>
-              </div> */}
-              <ListOfDatasets
-                options={options}
-                setOptions={setOptions}
-                ckan={ckan}
-              />{" "}
             </div>
           </div>
         </div>
